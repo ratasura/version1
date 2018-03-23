88518b9fc18b9c5a19777d165204a12558fc41b3
ratasura <tiburcio2409@gmail.com>
2018-03-23 14:33:22 -0500

    version 1.0

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'To-Do Application',
+    'description': 'Manage your personal Tasks with this module.',
+    'author': 'Luis Mario',
+    'depends': ['base'],
+    'application': True,
+    'data': ['views/todo_menu.xml'],
+}
